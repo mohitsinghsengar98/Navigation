9caedf46dbde35a1ed20ad087931831e8a8d8cc0
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-12-01 20:19:50 +0530

    How we use the path with navigation stack and navigation destination.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,26 +7,24 @@
 
 import SwiftUI
 
-struct DetailView:View {
-    let number: Int
-    
-    var body: some View {
-        Text("Detail View \(number)")
-    }
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating the detail View \(number)")
-    }
-}
-
 struct ContentView: View {
+    @State private var path = [Int]()
     var body: some View {
-        NavigationStack{
-            List(0..<1000){ i in
-                NavigationLink("Select \(i)",value: i)
-            }.navigationDestination(for: Int.self){ selection in  // by using the navigation destination we are creating the view only when it is called. not when we are showing the /setupping this navigation link.
-                DetailView(number:selection)
+        NavigationStack(path:$path){
+            VStack{
+                Button("show 32"){
+                    path = [32]
+                }
+                
+                Button("show 64"){
+                    path.append(64)
+                }
+                
+                Button("show 32 and 64"){
+                    path = [32,64]
+                }.navigationDestination(for: Int.self){ selection in
+                    Text("Your selection \(selection)")
+                }
             }
         }
     }

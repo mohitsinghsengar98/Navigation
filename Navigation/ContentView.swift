3c3ff5a820b26de7c029df1579435d2f6895f162
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-12-01 19:51:47 +0530

    Solution to dynamic navigation link view generating while showing the navigtaion link we use the navigation destination and generate the views only at the time of the use not while showing the navigation links.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -24,9 +24,9 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             List(0..<1000){ i in
-                NavigationLink("Tap Me"){ // in this it will create the multiple views when creating the this view.
-                    DetailView(number: i) // this view created before showing the navigation link run and check the same.
-                }
+                NavigationLink("Select \(i)",value: i)
+            }.navigationDestination(for: Int.self){ selection in  // by using the navigation destination we are creating the view only when it is called. not when we are showing the /setupping this navigation link.
+                Text("You selected \(selection)")
             }
         }
     }
